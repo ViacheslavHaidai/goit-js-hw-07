4a4f63806ab3782fbbddb93f6080f6e2212c4499
ViacheslavHaidai <145702643+ViacheslavHaidai@users.noreply.github.com>
2025-03-08 17:11:13 +0100

    task-1 done

--- js/task-1.js
+++ js/task-1.js
@@ -5,8 +5,13 @@ categoriesList.forEach((category) => {
   console.log(`Category: ${category.querySelector("h2").textContent}`);
   console.log(`Elements ${category.querySelectorAll("li").length}`);
 });
-const categoriesClass = document.querySelector("#categories");
-const newClass = categoriesClass.querySelectorAll("li");
-newClass.forEach((addClass) => {
+const categoriesClass = document.querySelectorAll("#categories .item ul li");
+
+categoriesClass.forEach((addClass) => {
   addClass.classList.add("item-child");
+});
+const listClass = document.querySelectorAll("#categories .item ul");
+
+listClass.forEach((addClass) => {
+  addClass.classList.add("list-items");
 });
