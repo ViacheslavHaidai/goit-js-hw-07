baad49ac5ff0d8cb2d8237d04f9c234eb47ae784
ViacheslavHaidai <145702643+ViacheslavHaidai@users.noreply.github.com>
2025-03-09 05:11:16 +0100

    task-4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const email = event.target.elements.email;
+  const password = event.target.elements.password;
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    alert(`All form fields must be filled in`);
+  } else {
+    const dataForm = {
+      email: email.value.trim(),
+      password: password.value.trim(),
+    };
+    console.log(dataForm);
+  }
+  form.reset();
+}
